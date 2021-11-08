8fa3fc659eb8f55970337ed9a73faec769af2902
tejaswini kakade <92703176+tejaswinikakade@users.noreply.github.com>
2021-11-09 01:21:45 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Hash_Table_Day15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //UC1
+
+            UC1_LineFrequency hash1 = new UC1_LineFrequency();
+            hash1.Freq();
+
+            //UC2
+
+            UC2_LargePara hash2 = new UC2_LargePara();
+            hash2.FreqLarge();
+
+            //UC3
+
+            UC3_Remove hash3 = new UC3_Remove();
+            hash3.removeWord("");
+        }
+    }
+}
